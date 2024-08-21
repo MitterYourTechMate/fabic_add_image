978be276a37b77533e938d6b8c0b53fc617431c1
shankey raheja <shankeyraheja@gmail.com>
2024-08-21 21:54:02 +1200

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,52 @@
 import logo from './logo.svg';
 import './App.css';
+import * as fabric from "fabric"
+import { useRef } from 'react';
+
 
 function App() {
+  const canvasref = useRef()
+  const handleAddImage = (e) => {
+    const canvas = new fabric.Canvas(canvasref.current);
+    let imgObj = e.target.files[0];
+    let reader = new FileReader();
+    reader.readAsDataURL(imgObj);
+    reader.onload = (e) => {
+      let imageUrl = e.target.result
+      let imageElement = document.createElement('img');
+      imageElement.src = imageUrl
+      imageElement.onload = function (){
+        let image = new fabric.Image(imageElement)
+        canvas.add(image)
+        canvas.centerObject(image)
+        canvas.setActiveObject(image)
+      }
+    }
+
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+       <input 
+        type="file" 
+        accept="image/*"
+        label="Add Image"
+        style={{
+          position:"absolute", 
+          top:"25%", 
+          left:"30%"}}
+          onChange={handleAddImage}
+        />
+        <canvas
+          width="300"
+          height="300"
+          style={{
+            border:"1px dotted white", 
+            position:"absolute", 
+            top:"30%", 
+            left:"30%"}}  
+          ref={canvasref}
+        />
+
     </div>
   );
 }
